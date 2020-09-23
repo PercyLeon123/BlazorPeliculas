f40bc1ddd6e8959f998915550d0c4591492d2a0c
Percy Leon <pleon@aysperu.com>
2020-09-23 15:57:11 -0500

    Agregando Generos

---
+++ BlazorPeliculas/Shared/Entidades/PeliculaActor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BlazorPeliculas.Shared.Entidades
+{
+    public class PeliculaActor
+    {
+        public int PersonaId { get; set; }
+        public int PeliculaId { get; set; }
+        public Persona Persona { get; set; }
+        public Pelicula Pelicula { get; set; }
+        public string Personaje { get; set; }
+        public int Orden { get; set; }
+    }
+}
